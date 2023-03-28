d7db52133f9df1a559641502fcef9826b610e866
joaoalpoim <joao.alpoim@qersolutions.com>
2023-03-28 11:58:52 +0100

    Adicionar aplicacao exemplo

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./Keycloak";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import WelcomePage from "./pages/Homepage";
+import SecuredPage from "./pages/Securedpage";
+import PrivateRoute from "./helpers/PrivateRoute";
+
+function App() {
+ return (
+   <div>
+     <ReactKeycloakProvider authClient={keycloak}>
+     <React.StrictMode>
+       <Nav />
+       <BrowserRouter>
+         <Routes>
+           <Route exact path="/" element={<WelcomePage />} />
+           <Route
+             path="/secured"
+             element={
+               <PrivateRoute>
+                 <SecuredPage />
+               </PrivateRoute>
+             }
+           />
+         </Routes>
+       </BrowserRouter>
+       </React.StrictMode>
+     </ReactKeycloakProvider>
+   </div>
+ );
+}
+
+export default App;
